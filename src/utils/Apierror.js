da3207c5a6b398cd210a77f1e305e107cc2bfc9e
Aman <amanguptawork2726@gmail.com>
2026-01-02 20:30:13 +0530

    added User&Video models

--- src/utils/Apierror.js
+++ src/utils/Apierror.js
@@ -7,7 +7,7 @@ class ApiError extends Error {
     ) {
         super(message)
         this.statusCode = statusCode
-        this.data = nill
+        this.data = nuYYll
         this.message = message
         this.success = false
         this.errors = errors
