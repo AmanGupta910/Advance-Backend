95ab1a7cd0e495aa602abf24960233b6e68d5ac3
Aman <amanguptawork2726@gmail.com>
2026-01-03 22:04:45 +0530

    minor Debugging and   make proper work flow

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
 
- const app= express();
+const app = express();
 
 
 
@@ -12,16 +12,22 @@ app.use(cors({
 }))
 
 
-                                                                                                                                                                
-
-app.use(express.json({limit: " 16kb"}))
-app.use(express.urlencoded({extended: true,limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
 
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)  //  http://localhost:8000/api/v1/users/register
 
 
 
- export {app}
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,19 @@
-
-// require('dotenv').config({path:'/.env'})
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/index.js"
-
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path:'/.env'
+    path: './.env'
 })
 
- connectDB()
- .then(()=>{
-    app.listen(process.env.PORT|| 8000,()=>{
-        console.log(` App running on ${process.env.PORT}`)
-        
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : ${process.env.PORT}`);
     })
- }).catch((error)=>{
-    console.log("Mongo_DB connection failed",error)
- })
-
-
-
-/*  
-import express from "express"
-const app = express()
-
-
-
-    (async () => {
-        try {
-            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-            app.on("error", (error) => {
-                console.log("ERROR", error);
-                throw error
-            })
-            app.listen(process.env.PORT), () => {
-                console.log(`Listing on port ${process.env.PORT}`)
-            }
-        }
-        catch (error) {
-            console.error(error)
-            throw error
-        }
-    })()
-        */
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
